cd5534e76e0214c883f756229b97ea125ed0553a
FakhraAslam <fakhraaslam1221@gmail.com>
2020-10-11 10:40:48 -0700

    here comes Implicit Intent means to say open another third party app in our app . so what i did is created a buton and editbox so that user add a ur; and intent help to open that .then i put a intent in code and add two parameters action view and uri .

--- app/src/main/java/com/project2/intentanddynamicview/MainActivity.java
+++ app/src/main/java/com/project2/intentanddynamicview/MainActivity.java
@@ -3,9 +3,12 @@ package com.project2.intentanddynamicview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
                 //1st is class jis m ham hn uska context
                 //2nd: jiss class m jana h uska context
                 startActivity(resultActivity); // startActivity m ham n apna intent jo create kia hoga pass krdena h
+
+                //Now Implicit Intent
+                Button btnGetURL;
+                final EditText etGetUrl;
+                etGetUrl=findViewById(R.id.et_get_url);
+                btnGetURL=findViewById(R.id.btn_get_url);
+                btnGetURL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"running",Toast.LENGTH_LONG);
+                       String url=etGetUrl.getText().toString();
+                       Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                       /*Parameters
+                       1st : URLs k liy actionView jo k intent ki class s h usy dety hein
+                       2nd : uri pass krna hota h hmary pass chunk wo string format m h user s string format m get kia h to usy
+                              uri m parse kr k dalein gay*/
+                       startActivity(intent);
+                    }
+                });
 
             }
         });
